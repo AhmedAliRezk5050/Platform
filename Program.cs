459f42b6be28948c902fd30c4959dfdb297dd95b
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-12 16:08:21 +0200

    first - lambda function middleware

---
+++ Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == HttpMethods.Get
+    && context.Request.Query["custom"] == "true")
+    {
+        Console.WriteLine("----------------if");
+        context.Response.ContentType = "text/plain";
+        await context.Response.WriteAsync("Written");
+    }
+    await next();
+});
+
+app.Run();
+
+
+// services are objects that provide features.
+// Managed by asp.net core.
+// DI makes it easy to access services in the application
