eb98f96a92dabe2235eb145861832c9cad56a4b5
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-12 20:54:05 +0200

    Pipeline Branches

--- Program.cs
+++ Program.cs
@@ -2,44 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Use(async (context, next) =>
-{
-    await next();
-    await context.Response
-    .WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
-});
-
-app.Use(async (context, next) =>
-{
-    if (context.Request.Path == "/short")
-    {
-        await context.Response
-        .WriteAsync($"Request Short Circuited");
-    }
-    else
-    {
-        await next();
-    }
-});
-
-app.Use(async (context, next) =>
-{
-    if (context.Request.Method == HttpMethods.Get
-    && context.Request.Query["custom"] == "true")
-    {
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("lambda function middleware \n");
-    }
-    await next();
+app.Map("/branch", branch => {
+    branch.UseMiddleware<Platform.QueryStringMiddleWare>();
+    branch.Use(async (HttpContext context, Func<Task> next) => {
+        await context.Response.WriteAsync($"Branch Middleware");
+    });
 });
 
 app.UseMiddleware<Platform.QueryStringMiddleWare>();
 
+app.MapGet("/", () => "Hello World!");
+
 app.Run();
-
-
-// services are objects that provide features.
-// Managed by asp.net core.
-// DI makes it easy to access services in the application
