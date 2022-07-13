b8a327e96768be515106ba21e2a918a12d8b4d31
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-13 21:16:44 +0200

    routing - Segment Variables

--- Program.cs
+++ Program.cs
@@ -4,13 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("routing", async context =>
+app.MapGet("{f}/{s}/{t}", async context =>
 {
-    await context.Response.WriteAsync("Request Was Routed");
+    await context.Response.WriteAsync("Request Was Routed\n");
+
+    foreach (var item in context.Request.RouteValues)
+    {
+        await context.Response.WriteAsync($"{item.Key} - {item.Value}\n");
+    }
 });
 
 app.MapGet("capital/uk", new Capital().Invoke);
 
 app.MapGet("population/paris", new Population().Invoke);
+
 
 app.Run();
