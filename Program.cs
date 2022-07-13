ea8d2d63d8c79f8bf9aabf12ca5945f159fb4bd0
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-13 12:00:58 +0200

    Options Pattern with Class-Based Middleware

--- Program.cs
+++ Program.cs
@@ -1,19 +1,33 @@
+using Microsoft.Extensions.Options;
+
 using Platform;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<MessageOptions>(options =>
+{
+    options.CityName = "Albany";
+});
+
+
 var app = builder.Build();
 
-app.Run(new QueryStringMiddleWare().Invoke);
-
-app.Run(async (context) => {
-    await context.Response.WriteAsync($"Run Middleware");
-});
-
-
-app.Run(async context =>
+app.MapGet("/location", async (HttpContext context,
+                                IOptions<MessageOptions> msgOpts) =>
 {
-    await context.Response.WriteAsync("Hello world!");
+    MessageOptions opts = msgOpts.Value;
+
+    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
 });
+
+
+//app.Run(async context =>
+//{
+//    await context.Response.WriteAsync("Hello world!");
+//});
+
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
