dca5ffb8ab1ab91a4b187f2c6a9b50908ec3e195
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-13 18:13:38 +0200

    routing - init

--- Program.cs
+++ Program.cs
@@ -1,33 +1,26 @@
-using Microsoft.Extensions.Options;
-
 using Platform;
-
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<MessageOptions>(options =>
-{
-    options.CityName = "Albany";
-});
-
-
 var app = builder.Build();
 
-app.MapGet("/location", async (HttpContext context,
-                                IOptions<MessageOptions> msgOpts) =>
-{
-    MessageOptions opts = msgOpts.Value;
+app.UseMiddleware<Population>();
 
-    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
+app.UseMiddleware<Capital>();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("routing",
+    async (context) => {
+        await context.Response.WriteAsync("Routed");
+    });
 });
 
-
-//app.Run(async context =>
-//{
-//    await context.Response.WriteAsync("Hello world!");
-//});
-
-app.MapGet("/", () => "Hello World!");
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Terminal Middleware Reached");
+});
 
 app.Run();
