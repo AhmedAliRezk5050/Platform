0e4e4a6d012526b8374c0660db01f1771d9e54a2
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-19 20:38:05 +0200

    dependency injection - Using an Adapter Function

---
+++ Services/EndpointExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Platform;
+using Platform.Services;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class EndpointExtensions
+    {
+        public static void MapWeather(this IEndpointRouteBuilder app, string path)
+        {
+            IResponseFormatter formatter = app.ServiceProvider
+                                              .GetRequiredService<IResponseFormatter>();
+
+            app.MapGet(path, context => WeatherEndpoint.Endpoint(context, formatter));
+        }
+    }
+}
