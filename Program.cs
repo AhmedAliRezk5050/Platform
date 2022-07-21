c6b8f9272e9d1fa3244d2558a8142bdb954e458c
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-21 15:07:57 +0200

    dependency injection - Dependency Chains

--- Program.cs
+++ Program.cs
@@ -4,22 +4,26 @@ using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IResponseFormatter, GuidService>();
+builder.Services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
+
+builder.Services.AddScoped<ITimeStamper, DefaultTimeStamper>();
 
 var app = builder.Build();
 
 app.UseMiddleware<WeatherMiddleware>();
 
-
-//app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter formatter) =>
-//{
-//    await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
-//});
+app.MapGet("middleware/function", 
+            async (HttpContext context,
+                   IResponseFormatter formatter) =>
+{
+    await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
+});
 
 //app.MapEndpoint<WeatherEndpoint>("endpoint/class");
 
 
-app.MapGet("endpoint/function", async (HttpContext context) => {
+app.MapGet("endpoint/function", async (HttpContext context) =>
+{
     IResponseFormatter formatter =
     context.RequestServices.GetRequiredService<IResponseFormatter>();
     await formatter.Format(context, "Endpoint Function: It is sunny in LA");
