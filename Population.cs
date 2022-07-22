1de45504ef6405252150a9056ac787e3edc7c990
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-23 00:10:18 +0200

    Using the Platform Features - Logging Messages with Attributes

--- Population.cs
+++ Population.cs
@@ -1,11 +1,11 @@
 ﻿namespace Platform
 {
-    public class Population
+    public partial class Population
     {
         public static async Task Endpoint(HttpContext context,
                                           ILogger<Population> logger)
         {
-            logger.LogDebug($"Started processing for {context.Request.Path}");
+            StartingResponse(logger, context.Request.Path);
 
             string city
                 = context.Request.RouteValues["city"] as string ?? "london";
@@ -36,5 +36,8 @@
 
             logger.LogDebug($"Finished processing for {context.Request.Path}");
         }
+
+        [LoggerMessage(0, LogLevel.Debug, "Starting response for {path}")]
+        public static partial void StartingResponse(ILogger logger, string path);
     }
 }
