c6907d2b87b6f96618f67f4ce3e587eb762064ff
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-22 16:36:01 +0200

    Using the Platform Features - Reading configuration data

--- Program.cs
+++ Program.cs
@@ -2,7 +2,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", async context => {
+app.MapGet("config", async (HttpContext context, IConfiguration config) =>
+{
+    string defaultDebug = config["Logging:LogLevel:Default"];
+
+    await context.Response
+    .WriteAsync($"The config setting is: {defaultDebug}");
+});
+
+app.MapGet("/", async context =>
+{
     await context.Response.WriteAsync("Hello World!");
 });
 
