739d73e065c92b68973a08fc21186415e16310ac
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-22 17:27:58 +0200

    Using the Platform Features - Using configuration data in the options pattern

---
+++ LocationMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Options;
+using Platform.Services;
+
+namespace Platform
+{
+    public class LocationMiddleware
+    {
+        private RequestDelegate next;
+
+        private readonly MessageOptions messageOptions;
+
+        public LocationMiddleware(RequestDelegate nextDelegate,
+            IOptions<MessageOptions> options)
+        {
+            next = nextDelegate;
+
+            messageOptions = options.Value;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path == "/location")
+            {
+                await context.Response.WriteAsync($"{messageOptions.CityName}, {messageOptions.CountryName}");
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Platform;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var servicesConfig = builder.Configuration;
+
+builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
+
 var app = builder.Build();
+
+var pipelineConfig = app.Configuration;
+
+app.UseMiddleware<LocationMiddleware>();
 
 app.MapGet("config", async (HttpContext context, IConfiguration config) =>
 {
