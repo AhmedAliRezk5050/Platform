772e6cb79d734f2f98053a0bfe85feccb2ee3db3
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-23 00:25:10 +0200

    Using the Platform Features - Logging HTTP Requests and Responses

--- Program.cs
+++ Program.cs
@@ -3,15 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-var logger = app.Services
-.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
-
-logger.LogDebug("Pipeline configuration starting");
+app.UseHttpLogging();
 
 app.MapGet("population/{city?}", Population.Endpoint);
-
-app.Logger.LogDebug("Pipeline configuration complete");
-
-logger.LogDebug("Pipeline configuration complete");
 
 app.Run();
