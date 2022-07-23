1191ca367e594a00661daf77a2a95f9e8df2988b
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-24 01:11:30 +0200

    Using the Platform Features - Enriching Status Code Responses

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ if (!app.Environment.IsDevelopment())
     app.UseStaticFiles();
 }
 
+app.UseStatusCodePages("text/html", Platform.Responses.DefaultResponse);
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/error")
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+
+        await Task.CompletedTask;
+    }
+    else
+    {
+        await next();
+    }
+});
+
 app.Run(context =>
 {
     throw new Exception("Something has gone wrong");
