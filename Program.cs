eebb3ebcdd7367a35db566192b7929d609011901
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-07-25 03:53:42 +0200

    Working with data - uncached response

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", async context => {
+app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
+
+app.MapGet("/", async context =>
+{
     await context.Response.WriteAsync("Hello World!");
 });
 
